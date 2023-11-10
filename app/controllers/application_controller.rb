64589905fada3699e2dfd9ad83c89ae836eec80f
elephantinforest <sikurowaiyado@gmail.com>
2023-11-10 10:08:45 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-    def hello
-    render html: "hola, mundo!"
-    end
 
-    def goodbye
-    render html: "「goodbye, world!」"
-    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,6 +11,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # resources :ControllerNameでRailsで定義されている7アクションのルーティングを設定
 
-   root "users#index"
+  #  root "users#index"
   # root "articles#index"
 end
